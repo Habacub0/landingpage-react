a73853287e4db463c4b32eaaa2fa873d3438b33c
Aaron Habacub Salguero Ceron <salguero.aaron@gmail.com>
2026-09-16 19:26:13 -0600

    Cambios en root para nuevo host

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/landingpage-react">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
